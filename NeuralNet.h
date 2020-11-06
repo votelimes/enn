d973e41f25a8790ec350b67bdf171284ad1d89d1
Ilya Semiletov <52174909+mcw1999@users.noreply.github.com>
2020-11-06 06:31:32 +0400

    Update and rename NeuralNet/Core/Main/NeuralNet.h to NeuralNet.h

---
+++ NeuralNet.h
@@ -0,0 +1,8 @@
+#ifndef NEURAL_NET_H_
+#define NEURAL_NET_H_
+
+#include "Net.h"
+#include "NetApplicationInterface.h"
+
+
+#endif // NEURAL_NET_H_
--- NeuralNet/Core/Main/NeuralNet.h
+++
@@ -1,8 +0,0 @@
-#ifndef NEURAL_NET_H_
-#define NEURAL_NET_H_
-
-#include "R:\Projects\NeuralNet\NeuralNet\Core\Net.h"
-#include "R:\Projects\NeuralNet\NeuralNet\Interface\NetApplicationInterface.h"
-
-
-#endif // NEURAL_NET_H_
