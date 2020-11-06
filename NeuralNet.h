3a50c2404f029a716414b460809d28e5d4fa6414
Ilya Semiletov <52174909+mcw1999@users.noreply.github.com>
2020-11-06 06:38:14 +0400

    Update NeuralNet.h

--- NeuralNet.h
+++ NeuralNet.h
@@ -1,8 +1,8 @@
 #ifndef NEURAL_NET_H_
 #define NEURAL_NET_H_
 
-#include "Net.h"
-#include "NetApplicationInterface.h"
+#include "net.h"
+#include "net_application_interface.h"
 
 
 #endif // NEURAL_NET_H_
