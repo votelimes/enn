19a16db31255dfb70e35f7cbe861dbe46e731f08
Ilya Semiletov <semiletov1@inbox.ru>
2020-11-27 03:01:22 +0400

    project structure fixed

--- inc/anet_core.h
+++ inc/anet_core.h
@@ -2,6 +2,10 @@
 #define _ANET_CORE_H_
 
 #include <src/core/net.h>
+#include <src/image_processing/image_processing.h>
+#include <src/interface/application_interface.h>
+
+#include <bitset>
 
 
 #endif // _ANET_CORE_H_
--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
-	network_core::NeuralNet net_core(64*64, 40, 10, 1);
+	network_core::NeuralNet net_core(8, 5, 2, 1);
+
+	auto number = 51;
+	auto binary_string = std::bitset<8>(number).to_string();
+
+	std::vector<int> binary_set;
+
+	std::for_each(binary_string.begin(), binary_string.end(), [&binary_set] (auto &symbol)
+	{
+		binary_set.push_back(symbol - '0');
+	});
 
 	return 0;
 }
